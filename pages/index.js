1be823052aa96270fadcd0b27ac7005091fed10d
mcdwayne <1dwayne.mcdaniel@gmail.com>
2020-11-11 11:35:03 -0600

    intial commit

--- pages/index.js
+++ pages/index.js
@@ -4,45 +4,40 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>I am up top</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          I made this site in 15 minutes total with <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Not joking, first time ever!  
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
+        
           <a href="https://nextjs.org/learn" className="card">
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Sure it does not do much!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>And I am not sure how to blog like I do with Hugo yet.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>But here it is!</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              and it deploys fine!
             </p>
           </a>
         </div>
